7e10bcd64b6c3f4a49d5edeabc0e050818e7580f
Marcin Pietras <martinpie10@gmail.com>
2020-11-17 15:24:54 +0100

    update BMI component

--- src/app/components/BMI/bmi.component.ts
+++ src/app/components/BMI/bmi.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NavigationService} from '../../services/navigation.service';
-import {FormControl, FormGroup} from '@angular/forms';
-
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -11,20 +10,43 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class BMIComponent implements OnInit, OnDestroy {
   bmiForm: FormGroup;
-  constructor(private navigateService: NavigationService) {
+  bmi = 0;
+  constructor(private navigateService: NavigationService, private fb: FormBuilder) {
   }
+
   ngOnInit(): void {
     this.navigateService.changeNavSubject(4);
+    this.buildForm();
+  }
+
+  // budownaie formularza startowego - pobieranie danych do bmi
+  buildForm() {
     this.bmiForm = new FormGroup({
-      weight: new FormControl(),
-      height: new FormControl(),
-      man: new FormControl(),
-      woman: new FormControl(),
+      weight: new FormControl(null, Validators.required),
+      height: new FormControl(null, Validators.required),
+      man: new FormControl(false),
+      woman: new FormControl(false),
     });
   }
-  onSubmit() {
-    console.log(this.bmiForm.value);
+
+  // odznacz inne checkboxy przy zaznaczeniu jednego
+  changeOtherCheckbox(value) {
+    if (value === 'man') {
+      this.bmiForm.get('woman').setValue(false);
+    } else {
+      this.bmiForm.get('man').setValue(false);
+    }
   }
+
+  // oblicz BMI
+  calcBMI(height, weight) {
+    return (weight / (height / 100 * height / 100)).toPrecision(4);
+  }
+
+  onSubmit() {
+    this.bmi = +this.calcBMI(this.bmiForm.get('height').value, this.bmiForm.get('weight').value);
+  }
+
   ngOnDestroy(): void {
   }
 
