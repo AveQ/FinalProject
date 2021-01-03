a23e8d3da2ba55990b9b5bc070bb619e4ee654de
Marcin Pietras <martinpie10@gmail.com>
2021-01-03 20:05:33 +0100

    cors heroku

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const port = process.env.PORT || 8080;
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/application'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/application/index.html'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Request-With,Content-Type,Accept,Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).json({});
+  }
+  next();
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/application'))
+app.get('/*', (req, res) => res.sendFile('index.html', {root: __dirname + '/dist/application'}));
+
+http.createServer(app)
+  .listen(port, () => {
+    console.log(`[Server] Server is running at https://localhost:${port}`);
+  });
